679bb0e81f825d055fdf053df64eb03aed501230
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-18 08:33:28 -0900

    multi select done

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,102 @@
-import z from "zod"
-import { useForm } from "react-hook-form"
-import { PasswordInput } from "./components/reuse/password-input"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./components/ui/form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button } from "./components/ui/button"
+import { Icons } from "./components/icons";
+import { MultiSelect } from "./components/reuse/multi-select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "./components/ui/button";
 
+const frameworksList = [
+  {
+    value: "next.js",
+    label: "Next.js",
+    icon: Icons.dog,
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+    icon: Icons.cat,
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+    icon: Icons.turtle,
+  },
+  {
+    value: "remix",
+    label: "Remix",
+    icon: Icons.rabbit,
+  },
+  {
+    value: "astro",
+    label: "Astro",
+    icon: Icons.fish,
+  },
+];
 
 function App() {
-  const Test = z.object({
-    password: z.string().min(1, {
-      message: "Password is requried."
-    })
-  })
+  const FormSchema = z.object({
+    frameworks: z
+      .array(z.string().min(1))
+      .min(1)
+      .nonempty("Please select at least one framework."),
+  });
 
-  const form = useForm<z.infer<typeof Test>>({
-    resolver: zodResolver(Test),
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
     defaultValues: {
-      password: ""
-    }
-  })
+      frameworks: ["next.js", "nuxt.js"],
+    },
+  });
 
-  const onSubmit = (values: z.infer<typeof Test>) => {
-      console.log(values)
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(`You have selected following frameworks: ${data.frameworks.join(", ")}.`);
+    
   }
 
   return (
-    <div className="p-6" >
-      <Form {...form} >
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-[20%]" >
-          <FormField 
-            name="password"
+    <div className="p-6">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-[500px]">
+          <FormField
             control={form.control}
-            render={({field})=> (
+            name="frameworks"
+            render={({ field }) => (
               <FormItem>
-                <FormLabel>Password</FormLabel>
+                <FormLabel>Frameworks</FormLabel>
                 <FormControl>
-                <PasswordInput {...field} placeholder="********" />
+                  <MultiSelect
+                    options={frameworksList}
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    placeholder="Select options"
+                    variant="inverted"
+                    animation={2}
+                    maxCount={3}
+                  />
                 </FormControl>
+                <FormDescription>
+                  Choose the frameworks you are interested in.
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-        <Button>Check</Button>
+          <Button variant="default" type="submit" className="w-full">
+            Submit
+          </Button>
         </form>
-
       </Form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
